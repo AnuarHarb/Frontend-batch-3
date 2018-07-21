eb2115d43b456e93d3c4f42da2a8865ccd820ece
Jonathan Coutiño Fuentes <ralts00@gmail.com>
2018-07-20 21:22:42 -0500

    se creo una pagina que despliega info del api de amiibos

--- ralts00/amiibos/assets/js/script.js
+++ ralts00/amiibos/assets/js/script.js
@@ -1,19 +1,27 @@
 'use strict'
-const newMovie = fetch('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22?api_key=8358b2f66408fb510fe420aabfe5683d');
+const amiiboApi = 'http://www.amiiboapi.com/api/amiibo/'
 const container = document.querySelector('#container');
 
-const name = (pokemon) => {
-  const pokemonName = document.createElement('h2');
-  pokemonName.innerText = pokemon.name;
-  container.appendChild(pokemonName);
-};
+fetch(amiiboApi)
+  .then(response => response.json())
+  .then(responseJson => createAmiibo(responseJson.amiibo))
 
-newMovie
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    console.log(response);
-  })
-
-const amiiboApi = 'http://www.amiiboapi.com/api/amiibo/'
+let createAmiibo = (amiibos) => {
+  for(let amiibo = 0; amiibo <= 100; amiibo ++) {
+    let classSeries = amiibos[amiibo].amiiboSeries.replace(/\s/g, '');
+    classSeries = classSeries.replace(/\./g, '');
+    classSeries = classSeries.replace(/-/g, '');
+    classSeries = classSeries.replace(/[0-9]/g, '');
+    classSeries = classSeries.replace(/'/g, '');
+    const card = `
+      <article class="card ${classSeries}">
+        <img src="${amiibos[amiibo].image}" class="card__image">
+        <span class="card__title">${amiibos[amiibo].name}</span>
+        <span class="card__info">${amiibos[amiibo].amiiboSeries}</span>
+        <span class="card__info">${amiibos[amiibo].gameSeries}</span>
+        <span class="card__info">${amiibos[amiibo].release.eu}</span>
+      </article>
+    `
+    container.innerHTML += card;
+  }
+}
